edb75c1365d8d5ed452a39633e37c00728001471
lobna <lobnazakaria63@yahoo.com>
2018-04-15 23:44:31 +0200

    modifying after login icons and searchbar alignment,also modifying alignment of logo image in homepage, and profile picture in userinfo

--- src/pages/user-info/user-info.ts
+++ src/pages/user-info/user-info.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 /**
  * Generated class for the UserInfoPage page.
@@ -15,11 +18,48 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class UserInfoPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad UserInfoPage');
-  }
+  profileData :  Observable<any>;
+   profileUrl:Observable<any>;
+   constructor(
+     public navCtrl: NavController,
+      public navParams: NavParams,
+      private fireAuth: AngularFireAuth,
+      public toastCtrl: ToastController,
+      private fireDatabase: AngularFireDatabase
+     ) {
+   }
+ 
+   getStyle(){
+     if (true){
+       const style = `display: none`;
+     return style;
+     }
+   }
+   ionViewDidLoad() {
+     console.log(this.profileData);
+     this.fireAuth.authState.take(1).subscribe(data =>{
+       if(data && data.email && data.uid){
+         debugger
+         this.toastCtrl.create({
+           message:`welcome to APP, ${data.email}`,
+           duration:3000
+         }).present();
+         this.profileData=this.fireDatabase.object('profile/'+data.uid).valueChanges();
+         
+         console.log(this.profileData,this.profileUrl)
+       }
+       else {
+         this.toastCtrl.create({
+           message:`couldnt find authentication detailes`,
+           duration:3000
+         }).present();
+       }
+     })
+   }
+ 
+   uploadImg(){
+     this.navCtrl.setRoot(UserInfoPage);
+   }
+ 
 
 }
