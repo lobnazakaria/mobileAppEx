4a14dd8af58790b55e3de2fc3a171910d8e02445
lobna <lobnazakaria63@yahoo.com>
2018-05-06 18:02:44 +0200

    adding create post button in footer page and adding some stuff in user info page

--- src/pages/post/post.ts
+++ src/pages/post/post.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**
- * Generated class for the FooterBarPage page.
+ * Generated class for the PostPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
@@ -10,18 +10,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-footer-bar',
-  templateUrl: 'footer-bar.html',
+  selector: 'page-post',
+  templateUrl: 'post.html',
 })
-export class FooterBarPage {
+export class PostPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad FooterBarPage');
+    console.log('ionViewDidLoad PostPage');
   }
-toPost(){
-  
-}
+
 }
